72af70bd60d68f1e75cc27994018f939c62f2704
Arseniy <Arseniystanovskoy@mail.ru>
2026-04-28 21:07:33 +0300

    Update ListSequence

--- include/ListSequence.h
+++ include/ListSequence.h
@@ -29,6 +29,46 @@ public:
     void Prepend(const T &item) override;
     void InsertAt(const T &item, int index) override;
     void RemoveAt(int index) override;
+
+    Sequence<T> *GetSubsequence(int startIndex, int endIndex) const override;
+
+    T &operator[](int index) override;
+    const T &operator[](int index) const override;
+
+    Sequence<T> *Map(std::function<T(const T &)> function) const override;
+    T Reduce(std::function<T(const T &, const T &)> function, const T &initial) const override;
+    Sequence<T> *Where(std::function<bool(const T &)> predicate) const override;
+    Sequence<T> *Concat(Sequence<T> *other) const override;
+    void Slice(int start, int count, Sequence<T> *insertSequence = nullptr) override;
+
+    void printSeq() const;
+
+    class ListSequenceIterator {
+    private:
+        const typename LinkedList<T>::Node* node_;
+    public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type = T;
+        using difference_type = std::ptrdiff_t;
+        using pointer = const T*;
+        using reference = const T&;
+
+        ListSequenceIterator(const typename LinkedList<T>::Node* node = nullptr);
+        reference operator*() const;
+        pointer operator->() const;
+        ListSequenceIterator& operator++();
+        ListSequenceIterator operator++(int);
+        ListSequenceIterator& operator--();
+        ListSequenceIterator operator--(int);
+        bool operator==(const ListSequenceIterator& other) const;
+        bool operator!=(const ListSequenceIterator& other) const;
+    };
+
+    using iterator = ListSequenceIterator;
+    using const_iterator = ListSequenceIterator;
+
+    const_iterator begin() const;
+    const_iterator end() const;
 };
 
 template<typename T>
@@ -135,5 +175,172 @@ void ListSequence<T>::RemoveAt(int index){
     }
     data = newList;
 }
+
+template<typename T>
+Sequence<T> *ListSequence<T>::GetSubsequence(int startIndex, int endIndex) const {
+    if (startIndex < 0 || endIndex >= GetLength() || startIndex > endIndex){
+        throw IndexOutOFBoundsException("Indexes out of range");
+    }
+    LinkedList<T> *sublist = data.GetSubList(startIndex, endIndex);
+    ListSequence<T> *result = new ListSequence<T>(*sublist);
+    delete sublist;
+    return result;
+}
+
+template<typename T>
+T &ListSequence<T>::operator[](int index) {
+    if (index < 0 || index >= GetLength()){
+        throw IndexOutOFBoundsException("Index out of range");
+    }
+    static T temp;
+    temp = data.Get(index);
+    return temp;
+}
+
+template<typename T>
+const T &ListSequence<T>::operator[](int index) const {
+    return const_cast<ListSequence<T> *>(this)->operator[](index);
+}
+
+template<typename T>
+Sequence<T> *ListSequence<T>::Map(std::function<T(const T &)> function) const {
+    ListSequence<T> *result = new ListSequence<T>();
+    for (int i = 0; i < GetLength(); i++) {
+        result->Append(function(data.Get(i)));
+    }
+    return result;
+}
+
+template<typename T>
+T ListSequence<T>::Reduce(std::function<T(const T &, const T &)> function, const T &initial) const {
+    T result = initial;
+    for (int i = 0; i < GetLength(); i++) {
+        result = function(result, data.Get(i));
+    }
+    return result;
+}
+
+template<typename T>
+Sequence<T> *ListSequence<T>::Where(std::function<bool(const T &)> predicate) const {
+    ListSequence<T> *result = new ListSequence<T>();
+    for (int i = 0; i < GetLength(); i++){
+        if (predicate(data.Get(i))){
+            result->Append(data.Get(i));
+        }
+    }
+    return result;
+}
+
+template<typename T>
+Sequence<T> *ListSequence<T>::Concat(Sequence<T> *other) const {
+    ListSequence<T> *result = new ListSequence<T>();
+    for (int i = 0; i < GetLength(); i++){
+        result->Append(data.Get(i));
+    }
+    for (int i = 0; i < other->GetLength(); i++){
+        result->Append(other->Get(i));
+    }
+    return result;
+}
+
+template<typename T>
+void ListSequence<T>::Slice(int start, int count, Sequence<T> *insertSequence) {
+    int length = GetLength();
+    if (start < 0){
+        start = length + start;
+    }
+    if (start < 0 || start >= length) {
+        throw IndexOutOFBoundsException("Start index out of range");
+    }
+    if (count < 0 || start + count > length) {
+        throw IndexOutOFBoundsException("Invalid count");
+    }
+
+    LinkedList<T> newData;
+    for (int i = 0; i < start; i++) {
+        newData.push_back(data.Get(i));
+    }
+    if (insertSequence != nullptr){
+        for (int i = 0; i < insertSequence->GetLength(); i++){
+            newData.push_back(insertSequence->Get(i));
+        }
+    }
+    for (int i = start + count; i < length; i++) {
+        newData.push_back(data.Get(i));
+    }
+    data = newData;
+}
+
+template<typename T>
+void ListSequence<T>::printSeq() const {
+    data.printLinkedList();
+}
+
+template<typename T>
+ListSequence<T>::ListSequenceIterator::ListSequenceIterator(const typename LinkedList<T>::Node* node)
+    : node_(node) {}
+
+template<typename T>
+typename ListSequence<T>::ListSequenceIterator::reference
+ListSequence<T>::ListSequenceIterator::operator*() const {
+    return node_->data_;
+}
+
+template<typename T>
+typename ListSequence<T>::ListSequenceIterator::pointer
+ListSequence<T>::ListSequenceIterator::operator->() const {
+    return &(node_->data_);
+}
+
+template<typename T>
+typename ListSequence<T>::ListSequenceIterator&
+ListSequence<T>::ListSequenceIterator::operator++() {
+    node_ = node_->next_;
+    return *this;
+}
+
+template<typename T>
+typename ListSequence<T>::ListSequenceIterator
+ListSequence<T>::ListSequenceIterator::operator++(int) {
+    ListSequenceIterator tmp = *this;
+    ++(*this);
+    return tmp;
+}
+
+template<typename T>
+typename ListSequence<T>::ListSequenceIterator&
+ListSequence<T>::ListSequenceIterator::operator--() {
+    node_ = node_->prev_;
+    return *this;
+}
+
+template<typename T>
+typename ListSequence<T>::ListSequenceIterator
+ListSequence<T>::ListSequenceIterator::operator--(int) {
+    ListSequenceIterator tmp = *this;
+    --(*this);
+    return tmp;
+}
+
+template<typename T>
+bool ListSequence<T>::ListSequenceIterator::operator==(const ListSequenceIterator& other) const {
+    return node_ == other.node_;
+}
+
+template<typename T>
+bool ListSequence<T>::ListSequenceIterator::operator!=(const ListSequenceIterator& other) const {
+    return !(*this == other);
+}
+
+template<typename T>
+typename ListSequence<T>::const_iterator ListSequence<T>::begin() const {
+    return const_iterator(data_.GetHead());
+}
+
+template<typename T>
+typename ListSequence<T>::const_iterator ListSequence<T>::end() const {
+    return const_iterator(nullptr);
+}
+
 
 #endif // LISTSEQUENCE_H
